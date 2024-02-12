5e125faeecf613190090ea6b410b8304453a9a0a
ImMoa <yumemiayumu@gmail.com>
2024-02-12 17:32:51 +0900

    Fix /maint error.

---
+++ src/removecmd.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const { REST, Routes } = require('discord.js');
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(process.env.BOT_TOKEN);
+
+const clientId = process.env.BOT_CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+const cmdId = '';
+
+// for guild-based commands
+rest.delete(Routes.applicationGuildCommand(clientId, guildId, cmdId))
+	.then(() => console.log('Successfully deleted guild command'))
+	.catch(console.error);
+
+// for global commands
+// rest.delete(Routes.applicationCommand(clientId, 'commandId'))
+// 	.then(() => console.log('Successfully deleted application command'))
+// 	.catch(console.error);
