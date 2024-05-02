a74e81b5e810bda8bc57be09bdfd258f4bb14a5b
Ayumudayo <yumemiayumu@gmail.com>
2024-05-02 23:34:06 +0900

    - /exchange can get decimal point - Remove some currency from /rate

--- src/removecmd.js
+++ src/removecmd.js
@@ -6,7 +6,7 @@ const rest = new REST().setToken(process.env.BOT_TOKEN);
 
 const clientId = process.env.BOT_CLIENT_ID;
 const guildId = process.env.GUILD_ID;
-const cmdId = '';
+const cmdId = '1203980508661547053';
 
 // for guild-based commands
 rest.delete(Routes.applicationGuildCommand(clientId, guildId, cmdId))
