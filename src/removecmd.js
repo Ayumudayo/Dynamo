1f48b4162eaf9fa6ac3686d99e92e246713dd384
Ayumudayo <yumemiayumu@gmail.com>
2024-11-20 19:46:20 +0900

    Update yahoo-finance2 module

--- src/removecmd.js
+++ src/removecmd.js
@@ -5,15 +5,18 @@ const { REST, Routes } = require('discord.js');
 const rest = new REST().setToken(process.env.BOT_TOKEN);
 
 const clientId = process.env.BOT_CLIENT_ID;
-const guildId = process.env.GUILD_ID;
-const cmdId = '1203980508661547053';
+const guildId = '';
+const cmdId = '';
+
+console.log(clientId)
+console.log(guildId)
 
 // for guild-based commands
-rest.delete(Routes.applicationGuildCommand(clientId, guildId, cmdId))
-	.then(() => console.log('Successfully deleted guild command'))
-	.catch(console.error);
+// rest.delete(Routes.applicationGuildCommand(clientId, guildId, cmdId))
+// 	.then(() => console.log('Successfully deleted guild command'))
+// 	.catch(console.error);
 
 // for global commands
-// rest.delete(Routes.applicationCommand(clientId, 'commandId'))
-// 	.then(() => console.log('Successfully deleted application command'))
-// 	.catch(console.error);
+rest.put(Routes.applicationCommands(clientId), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
+	.catch(console.error);
